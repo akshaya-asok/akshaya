69492267f985f159e7f2d3fe0e3ca0349c1d55f3
abdulazeezh1998 <77092497+abdulazeezh1998@users.noreply.github.com>
2021-01-07 12:24:54 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,41 +1,43 @@
-package com.example.check;
+package com.example.checklist2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    CheckBox c1, c2;
-    TextView t;
-    Button b1, b2;
-
+CheckBox cb1,cb2;
+Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        t = (TextView) findViewById((R.id.textView2));
-        b1 = (Button) findViewById((R.id.button));
-        b2 = (Button) findViewById((R.id.button2));
-        c1 = (CheckBox) findViewById(R.id.checkBox);
-        c2 = (CheckBox) findViewById(R.id.checkBox2);
+
+
+        cb1=(CheckBox)findViewById(R.id.cb1);
+        cb2=(CheckBox)findViewById(R.id.cb2);
+        b1=(Button)findViewById(R.id.b1);
+        b2=(Button)findViewById(R.id.b2);
+
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                c1.setChecked(true);
-                c2.setChecked(false);
-                t.setText("Check Box 1 is selected by button1");
+               cb1.setChecked(true);
+                cb2.setChecked(false);
+                Toast toast=Toast.makeText(getApplicationContext(),String.valueOf("Changing Checkbox 1 state"),Toast.LENGTH_SHORT);
+                toast.show();
             }
         });
+
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                c1.setChecked(false);
-                c2.setChecked(true);
-                t.setText("Check Box 2 is selected by button2");
+                cb2.setChecked(true);
+                cb1.setChecked(false);
+                Toast toast=Toast.makeText(getApplicationContext(),String.valueOf("Changing Checkbox 2 state"),Toast.LENGTH_SHORT);
+                toast.show();
             }
         });
     }
