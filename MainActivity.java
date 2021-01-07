da1a323d61b6674789cc680f1d00a28720310be0
ajinasr <77092559+ajinasr@users.noreply.github.com>
2021-01-07 12:19:01 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,40 +1,42 @@
-package com.example.timepicker;
+package com.example.check;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textview1;
-    TimePicker timepicker;
-    Button changetime;
+    CheckBox c1, c2;
+    TextView t;
+    Button b1, b2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textview1=(TextView) findViewById(R.id.textView1);
-        timepicker=(TimePicker)findViewById(R.id.timePicker);
-        //Uncomment the below line of code for 24 hour view
-        timepicker.setIs24HourView(true);
-        changetime=(Button)findViewById(R.id.button1);
-
-        textview1.setText(getCurrentTime());
-        changetime.setOnClickListener(new View.OnClickListener(){
+        t = (TextView) findViewById((R.id.textView2));
+        b1 = (Button) findViewById((R.id.button));
+        b2 = (Button) findViewById((R.id.button2));
+        c1 = (CheckBox) findViewById(R.id.checkBox);
+        c2 = (CheckBox) findViewById(R.id.checkBox2);
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                textview1.setText(getCurrentTime());
+            public void onClick(View v) {
+                c1.setChecked(true);
+                c2.setChecked(false);
+                t.setText("Check Box 1 is selected by button1");
             }
         });
-
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                c1.setChecked(false);
+                c2.setChecked(true);
+                t.setText("Check Box 2 is selected by button2");
+            }
+        });
     }
-    public String getCurrentTime(){
-        String currentTime="Current Time: "+timepicker.getCurrentHour()
-                +":"+timepicker.getCurrentMinute();
-        return currentTime;
-    }
-
 }
